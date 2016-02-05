3b94370d9e655b6cc859f66daa0420251162fadc
nikopapp <np15685my.bristol.ac.uk>
2016-02-05 18:38:58 +0000

    05-02-2016/18:38

--- myscripts.js
+++ myscripts.js
@@ -1,8 +1,12 @@
 function changeImage() {
     var image = document.getElementById('myImage');
-    if (image.src.match("elva1")) {
-        image.src = "Client/images/content/elva2.jpg";
+    var imagefolder="Client/images/content/";
+    var imageExt=".jpg";
+    var imageNum= /\d+$/.exec(image.src);
+    var imageInt=parseInt(imageNum);
+    if (image.src.match("Client/images/content/1.jpg")) {
+        image.src = imagefolder+imageNum+imageExt;
     } else {
-        image.src = "Client/images/content/elva1.jpg";
+        image.src = "Client/images/content/1.jpg";
     }
 }
