6642d5b1399069130fa135a9bff41e6f1f3ff964
nikopapp <np15685my.bristol.ac.uk>
2016-03-03 10:45:03 +0000

    03-03-2016/10:45

--- client/animation.js
+++ client/animation.js
@@ -4,6 +4,12 @@ var animate = function() {
     var image3 = document.getElementById('drop3');
     var image4 = document.getElementById('drop4');
     var image5 = document.getElementById('drop5');
+    var image6 = document.getElementById('drop6');
+    var image7 = document.getElementById('drop7');
+    var image8 = document.getElementById('drop8');
+    var image9 = document.getElementById('drop9');
+    var image10 = document.getElementById('drop10');
+
     var mainImage = document.getElementById('logo');
     var maintitle = document.getElementById('titleEAT');
     var animDiv = document.getElementsByClassName('animation');
@@ -25,28 +31,51 @@ var animate = function() {
     image4.style.left = (posX+5) +"%";
     image5.style.top = (posY+11) +"%";
     image5.style.left = (posX+11) +"%";
+    image6.style.top = (posY+10) +"%";
+    image6.style.left = (posX+6) +"%";
+    image7.style.top = (posY+9) +"%";
+    image7.style.left = (posX+10) +"%";
+    image8.style.top = (posY+10) +"%";
+    image8.style.left = (posX+9) +"%";
+    image9.style.top = (posY+16) +"%";
+    image9.style.left = (posX+5) +"%";
+    image10.style.top = (posY+14) +"%";
+    image10.style.left = (posX+14) +"%";
     var id = setInterval(frame, 250);
       function frame() {
             if (maintitle.style.opacity == 1) {
                clearInterval(id);
            } else if (counter== 0){
-             image1.style.opacity = 1;
+             image1.style.opacity = 0.8;
            } else if (counter == 2){
-             image2.style.opacity = 1;
+             image2.style.opacity = 0.8;
            }
            else if (counter == 3){
-             image3.style.opacity = 1;
+             image3.style.opacity = 0.8;
            }
            else if (counter == 6){
-             image4.style.opacity = 1;
+             image4.style.opacity = 0.81;
            }
            else if (counter == 8){
-             image5.style.opacity = 1;
+             image5.style.opacity = 0.81;
+           } else if (counter== 10){
+             image1.style.opacity = 0.8;
+           } else if (counter == 11){
+             image2.style.opacity = 0.8;
            }
-           else if(counter ==10){
-             mainImage.style.opacity = 1;
+           else if (counter == 13){
+             image3.style.opacity = 0.8;
            }
-           else if(counter ==11){
+           else if (counter == 16){
+             image4.style.opacity = 0.81;
+           }
+           else if (counter == 18){
+             image5.style.opacity = 0.81;
+           }
+           else if(counter ==21){
+             mainImage.style.opacity = 0.81;
+           }
+           else if(counter ==22){
              maintitle.style.opacity = 1;
            }
            counter++;
