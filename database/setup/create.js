7c1a2400dc0413a62127f1fed77c74c5594cb4d0
nikopapp <np15685my.bristol.ac.uk>
2016-05-17 12:15:53 +0100

    17-05-2016/12:15

--- database/setup/create.js
+++ database/setup/create.js
@@ -1,7 +1,7 @@
 "use strict";
 var sql = require("sqlite3");
 sql.verbose();
-var db = new sql.Database("test.db");
+var db = new sql.Database("../test.db");
 db.serialize(startup);
 
 function startup() {
@@ -10,6 +10,13 @@ function startup() {
                            "email VARCHAR(100) NOT NULL,"+
                     "CONSTRAINT pkUid PRIMARY KEY(userId))",
          err);
+  db.run("CREATE TABLE Message (mesId INTEGER,"+
+                               "userId INTEGER,"+
+                               "body TEXT NOT NULL,"+
+                               "tstamp INTEGER,"+
+                               "CONSTRAINT pkPid PRIMARY KEY(userId)"+
+                               "CONSTRAINT fkUid FOREIGN KEY(userId) REFERENCES User(userId))",
+        err);       
   // db.run("insert into pets values ('Odie','dog')", err);
   // db.run("insert into pets values ('Wanda','fish')", err);
   db.close();
