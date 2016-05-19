c9d601f91c4f53efbcf5b8ed5c264fa2010afd03
blaza1990 <ab15236@my.bristol.ac.uk>
2016-05-19 12:03:06 +0100

    simple messages working

--- nodeScripts/comments_form.js
+++ nodeScripts/comments_form.js
@@ -14,44 +14,18 @@ module.exports.insertUser=function insertUser(query, db){
     index = querySplit[3].indexOf('=');
     var message = deleteAtSymbol(querySplit[3].substring(index+1,querySplit[3].length));
     console.log(message);
-    var strtemp = "INSERT INTO User(name,email) Values ('"+ uName +"','"+email+"')";
-    console.log(strtemp);
-    db.run(strtemp);
-    // insertMessage(message, db);
+    sqlrun(uName, email, message, db);
   }
+}
+
+function sqlrun(uName, email, message, db){
+  var unixTime = Math.round(new Date().getTime()/1000); // rounded to unix time
+  var strtemp = "INSERT INTO Message(name, tstamp, body, email) Values ('"+ uName +"','"+ unixTime +"','"+ message +"','"+email+"')";
+  console.log(strtemp);
+  db.run(strtemp);
 }
 
 function deleteAtSymbol(email){
   var re = /%40/g;
   return email.replace(re, "@");
 }
-
-function insertMessage(message, db){
-  if(query!=null){
-    var lastMessageSQL = "select messageID from User order by messageID desc limit 1;" //returns the last user in the list
-    // var lastMessage = db.run(lastMessageSQL);
-
-    var unixTime = new Date().getTime();
-    // var strtemp = "INSERT INTO Message(messageId, body, tstamp ) Values ('"+ userId +"','"+message+"','" + unixTime +"')";
-    // var consoleOUT="\n" + message + " time= " + unixTime + " " + " lastMessageID= " + lastMessage;
-    // console.log(consoleOUT);
-    // db.run(strtemp);
-  }
-}
-
-
-
-// module.exports.insertMessage=function insertMessage(query, db){
-//   if(query!=null){
-//     var querySplit = query.split('&');
-//     var lastUser = "select messageID from User order by messageID desc limit 1;" //returns the last user in the list
-//     var userId = db.run(lastUser);
-//     index = querySplit[3].indexOf('=');
-//     var message = deleteAtSymbol(querySplit[3].substring(index+1,querySplit[3].length));
-//     console.log(email);
-//     var unixTime = new Date().getTime();
-//     var strtemp = "INSERT INTO Message(messageId, body, tstamp ) Values ('"+ userId +"','"+message+"','" + unixTime +"')";
-//     console.log(strtemp);
-//     db.run(strtemp);
-//   }
-// }
