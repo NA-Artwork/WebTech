fc16c858497c6141117ab858926f0ecccdbc6de4
ab15236 <ab15236@bristol.ac.uk>
2016-05-21 15:21:00 +0100

    deleted unused code

--- nodeScripts/authenticate.js
+++ nodeScripts/authenticate.js
@@ -10,7 +10,7 @@ module.exports.verify=function verify(un, p, db){
   var ps = db.prepare("SELECT userName as username from User where username=?");
   ps.get(un, function(err, row) {
     if(row!=undefined){
-      userName=row.username;
+      userName=row.userName;
       console.log("un=" + un + "username=" + userName);
     }
     });
@@ -39,35 +39,3 @@ module.exports.verify=function verify(un, p, db){
   module.exports.getValue=function getValue(){
     return returnval;
   }
-
-  //
-  // function passMatch(passWordMatch, userNameMatch){
-  //   // console.log("passWordMatch userNameMatch=" + passWordMatch + " " + userNameMatch)
-  //   if(passWordMatch==true && userNameMatch==true){
-  //     console.log("full match");
-  //     return true;
-  //   }
-  //   else{
-  //     console.log("fail match");
-  //     return false;
-  //   }
-
-  // if(passWordMatch==true && userNameMatch==true){
-  //   console.log("full match");
-  //   return true;
-  // }
-  // else{
-  //   console.log("fail");
-  //   return false;
-  // }
-  // if(un==="Nikos"&&p==="pa") return true;
-  // else return false;
-
-
-
-
-
-// db.run("CREATE TABLE IF NOT EXISTS" +
-//        "User (usId INTEGER," +
-//         "userName VARCHAR(100) NOT NULL UNIQUE, pass VARCHAR(100) NOT NULL)"+
-//         "CONSTRAINT pkUid PRIMARY KEY(usId))", err);
